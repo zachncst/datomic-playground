91d5f022ef6e8dfd4935d53f7c3f301edc55a456
Zachary Taylor <zachncst@gmail.com>
2016-02-04 23:23:53 -0500

    Adding lein plugin

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.datomic/datomic-pro "0.9.5327"]]
+  :plugins [[lein-datomic "0.2.0"]]
   :main ^:skip-aot datomic-playground.core
   :target-path "target/%s"
   :profiles {:dev {:datomic {:config "resources/free-transactor-template.properties"
